099f619f86736778a9a1c21906f4c2b6875f5fd2
Nout Kleef <noutkleef1@gmail.com>
2022-02-07 15:15:25 +0000

    create synthetic scenes

--- synthetic-downsampler/synthetic_dataset_builder.py
+++ synthetic-downsampler/synthetic_dataset_builder.py
@@ -1,3 +1,9 @@
+import glob
+import os
+from pathlib import Path
+import shutil
+import tqdm
+
 class SyntheticDatasetBuilder(object):
     def __init__(self, args):
         self.load_path = args.load_path
@@ -6,16 +12,30 @@ class SyntheticDatasetBuilder(object):
         self.format = None
 
     def produce_dataset(self):
-        self._produce_lowres()
+        self._produce_training_data()
         # TODO: copy test dir
 
-    def _produce_lowres(self):
-        scenes = self.format.get_scene_paths()
-        print(f'Producing synthetic low resolution data for {len(scenes)} scenes')
-        for scene in scenes:
-            self._produce_lowres_for_scene(scene)
-
-    def _produce_lowres_for_scene(self, load_dir):
+    def _produce_training_data(self):
+        auth_scenes = self.format.get_scene_paths()
+        print(f'Producing synthetic low resolution data for {len(auth_scenes)} scenes')
+        for auth_scene in tqdm.tqdm(auth_scenes):
+            synth_scene = self._produce_synthetic_scene(auth_scene)
+            self._produce_lowres_images(synth_scene)
+            
+    def _produce_synthetic_scene(self, load_dir):
         save_dir = self.format.convert_load_path_to_save_path(load_dir)
-        # create scene dir
+        os.makedirs(save_dir, exist_ok=True)
+        # copy HR + SM
+        shutil.copy(Path(load_dir) / 'HR.png', save_dir)
+        shutil.copy(Path(load_dir) / 'SM.png', save_dir)
+        # produce LR images, copy quality maps
+        lrs = glob.glob(os.path.join(load_dir, 'LR*.png'))
+        for lr in lrs:
+            lr = Path(lr)
+            lr_idx = lr.name[2:-4]  # match /LR[0-9]+.png/
+            quality_map_path = lr.parent / f'QM{lr_idx}.png'
+            shutil.copy(quality_map_path, save_dir)
+        return save_dir
 
+    def _produce_lowres_images(self, scene):
+        pass  #TODO
