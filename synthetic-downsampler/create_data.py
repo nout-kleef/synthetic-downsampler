240758cb453f783907cec463fbc148e3e9243c41
Nout Kleef <noutkleef1@gmail.com>
2022-02-07 12:46:50 +0000

    add support for random seed

--- synthetic-downsampler/create_data.py
+++ synthetic-downsampler/create_data.py
@@ -6,9 +6,10 @@ def main():
     parser = argparse.ArgumentParser(description='Create a synthetic dataset from high-resolution ground truth.')
     parser.add_argument('load_path', help='Path to root directory containing HR images')
     parser.add_argument('save_path', help='Path to root of directory where to store produced LR images')
+    parser.add_argument('--random_seed', help='Random seed to make results reproducible')
     args = parser.parse_args()
     # produce a new dataset
-    dataset = SyntheticDataset(load_path=args.load_path, save_path=args.save_path)
+    dataset = SyntheticDataset(args)
     dataset.produce_lowres()
 
 if __name__ == '__main__':
--- synthetic-downsampler/synthetic_dataset.py
+++ synthetic-downsampler/synthetic_dataset.py
@@ -1,7 +1,8 @@
 class SyntheticDataset(object):
-    def __init__(self, load_path, save_path):
-        self.load_path = load_path
-        self.save_path = save_path
+    def __init__(self, args):
+        self.load_path = args.load_path
+        self.save_path = args.save_path
+        self.random_seed = args.random_seed
 
     def produce_lowres(self):
         pass
