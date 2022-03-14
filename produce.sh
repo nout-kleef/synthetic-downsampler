c7bb830b40bf7e80736e97447ee569ee88aea259
Nout Kleef <noutkleef1@gmail.com>
2022-03-14 16:35:39 +0000

    produce 100/100

--- produce.sh
+++ produce.sh
@@ -2,14 +2,14 @@
 
 SYNTHETIC_ROOT='/Users/nout/github/piunet/synthetic'
 
-# baseline
-# python synthetic-downsampler/create_data.py ~/datasets/piunet_probav/probav_data "${SYNTHETIC_ROOT}/probav_data1" 100 0.5 30
-# A
-python synthetic-downsampler/create_data.py ~/datasets/piunet_probav/probav_data "${SYNTHETIC_ROOT}/probav_data_A" 50 0.5 30
-zip -r "${SYNTHETIC_ROOT}/probav_data_A.zip" "${SYNTHETIC_ROOT}/probav_data_A"
-# B
-python synthetic-downsampler/create_data.py ~/datasets/piunet_probav/probav_data "${SYNTHETIC_ROOT}/probav_data_B" 100 0.25 30
-zip -r "${SYNTHETIC_ROOT}/probav_data_B.zip" "${SYNTHETIC_ROOT}/probav_data_B"
-# # C
-python synthetic-downsampler/create_data.py ~/datasets/piunet_probav/probav_data "${SYNTHETIC_ROOT}/probav_data_C" 100 0.5 15
-zip -r "${SYNTHETIC_ROOT}/probav_data_C.zip" "${SYNTHETIC_ROOT}/probav_data_C"
+# 100/100
+python synthetic-downsampler/create_data.py ~/datasets/piunet_probav/probav_data "${SYNTHETIC_ROOT}/probav_data1" 100 0.5 30 auth100 synth100
+# # A
+# python synthetic-downsampler/create_data.py ~/datasets/piunet_probav/probav_data "${SYNTHETIC_ROOT}/probav_data_A" 50 0.5 30
+# zip -r "${SYNTHETIC_ROOT}/probav_data_A.zip" "${SYNTHETIC_ROOT}/probav_data_A"
+# # B
+# python synthetic-downsampler/create_data.py ~/datasets/piunet_probav/probav_data "${SYNTHETIC_ROOT}/probav_data_B" 100 0.25 30
+# zip -r "${SYNTHETIC_ROOT}/probav_data_B.zip" "${SYNTHETIC_ROOT}/probav_data_B"
+# # # C
+# python synthetic-downsampler/create_data.py ~/datasets/piunet_probav/probav_data "${SYNTHETIC_ROOT}/probav_data_C" 100 0.5 15
+# zip -r "${SYNTHETIC_ROOT}/probav_data_C.zip" "${SYNTHETIC_ROOT}/probav_data_C"
